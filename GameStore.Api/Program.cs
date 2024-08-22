70cb35d9b4f848d182864a8efaaeed4d6365e43d
Dennis Vo <dennisphuc@gmail.com>
2024-08-22 11:09:44 +0200

    feat: refactor Program.cs, create extension method for WebApplication with MapGamesEndpoints

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,83 +1,9 @@
 using GameStore.Api.Dtos;
+using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-const string GetGameEndpointName = "GetGame";
-
-List<GameDto> games = [
-    new (
-        0, "item 0", "name item 0", 5.00M, new DateOnly(2020,9,30)
-    ),
-    new (
-        1, "item 1", "name item 1", 10.00M, new DateOnly(2020,10,30)
-    ),
-    new (
-        2, "item 2", "name item 2", 20.00M, new DateOnly(2020,11,30)
-    ),
-    new (
-        3, "item 3", "name item 3", 30.00M, new DateOnly(2020,12,30)
-    )
-];
-
-//GET /games
-app.MapGet("games", () => games);
-
-//GET /games/1
-app.MapGet("games/{id}", (int id) => {
-    GameDto? game = games.Find(g => g.Id == id);
-
-    return game is null? Results.NotFound() : Results.Ok(game);
-})
-.WithName(GetGameEndpointName);
-
-//POST /games
-app.MapPost("games", (CreateGameDto newGame) => {
-    GameDto game = new (
-        games.Count + 1,
-        newGame.Name,
-        newGame.Genre,
-        newGame.Price,
-        newGame.ReleaseDate
-    );
-
-    games.Add(game);
-
-    return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id}, game);
-});
-
-//PUT /games/1
-app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) => {
-    var index = games.FindIndex(g => g.Id == id);
-
-    if (index == -1)
-    {
-        return Results.NotFound();
-    }
-
-    games[index] = new GameDto(
-        id, 
-        updatedGame.Name, 
-        updatedGame.Genre, 
-        updatedGame.Price, 
-        updatedGame.ReleaseDate
-    );
-
-    return Results.NoContent();
-});
-
-//DELETE /games/1
-app.MapDelete("games/{id}", (int id) => {
-    var index = games.FindIndex(g => g.Id == id);
-
-    if (index == -1)
-    {
-        return Results.NotFound();
-    }
-
-    games.RemoveAll(g => g.Id == id);
-
-    return Results.NoContent();
-});
+app.MapGamesEndpoints();
 
 app.Run();
