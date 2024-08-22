e91f93960f88edb44ca7d9f27f3d8b807319148e
Dennis Vo <dennisphuc@gmail.com>
2024-08-22 11:02:20 +0200

    feat: update the Exception handling when not found the id in the request, GET, PUT, DELETE

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -24,7 +24,11 @@ List<GameDto> games = [
 app.MapGet("games", () => games);
 
 //GET /games/1
-app.MapGet("games/{id}", (int id) => games.Find(g => g.Id == id))
+app.MapGet("games/{id}", (int id) => {
+    GameDto? game = games.Find(g => g.Id == id);
+
+    return game is null? Results.NotFound() : Results.Ok(game);
+})
 .WithName(GetGameEndpointName);
 
 //POST /games
@@ -46,20 +50,18 @@ app.MapPost("games", (CreateGameDto newGame) => {
 app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) => {
     var index = games.FindIndex(g => g.Id == id);
 
-    if (index != -1)
+    if (index == -1)
     {
-        games[index] = new GameDto(
-            id, 
-            updatedGame.Name, 
-            updatedGame.Genre, 
-            updatedGame.Price, 
-            updatedGame.ReleaseDate
-        );
+        return Results.NotFound();
     }
-    else
-    {
-        return Results.BadRequest();
-    }
+
+    games[index] = new GameDto(
+        id, 
+        updatedGame.Name, 
+        updatedGame.Genre, 
+        updatedGame.Price, 
+        updatedGame.ReleaseDate
+    );
 
     return Results.NoContent();
 });
@@ -68,14 +70,12 @@ app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) => {
 app.MapDelete("games/{id}", (int id) => {
     var index = games.FindIndex(g => g.Id == id);
 
-    if (index != -1)
+    if (index == -1)
     {
-        games.RemoveAll(g => g.Id == id);
+        return Results.NotFound();
     }
-    else
-    {
-        return Results.BadRequest();
-    }
+
+    games.RemoveAll(g => g.Id == id);
 
     return Results.NoContent();
 });
