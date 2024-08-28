50bc0cc569aa3915ac18a827977751eda929d8b5
Dennis Vo <dennisphuc@gmail.com>
2024-08-28 11:44:35 +0200

    feat: using dependency injection in POST method, update CreateDTO

--- GameStore.Api/Dtos/CreateGameDto.cs
+++ GameStore.Api/Dtos/CreateGameDto.cs
@@ -5,8 +5,7 @@ namespace GameStore.Api.Dtos;
 public record class CreateGameDto(
     [Required][StringLength(50)]
     string Name, 
-    [Required][StringLength(20)]
-    string Genre, 
-    [Required][Range(1, 100)]
+    int GenreId, 
+    [Range(1, 100)]
     decimal Price,
     DateOnly ReleaseDate);
--- GameStore.Api/Endpoints/GamesEndpoints.cs
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -1,5 +1,7 @@
 using System;
+using GameStore.Api.Data;
 using GameStore.Api.Dtos;
+using GameStore.Api.Entities;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace GameStore.Api.Endpoints;
@@ -39,18 +41,29 @@ public static class GamesEndpoints
         .WithName(GetGameEndpointName);
 
         //POST /games
-        group.MapPost("/", (CreateGameDto newGame) => {
-            GameDto game = new (
-                games.Count + 1,
-                newGame.Name,
-                newGame.Genre,
-                newGame.Price,
-                newGame.ReleaseDate
+        group.MapPost("/", (CreateGameDto newGame, GameStoreContext dbContext) => {
+            
+            Game game = new Game()
+            {
+                Name = newGame.Name,
+                Genre = dbContext.Genres.Find(newGame.GenreId),
+                GenreId = newGame.GenreId,
+                Price = newGame.Price,
+                ReleaseDate = newGame.ReleaseDate
+            };
+
+            dbContext.Games.Add(game);
+            dbContext.SaveChanges();
+
+            GameDto gameDto = new GameDto(
+                game.Id,
+                game.Name,
+                game.Genre!.Name,
+                game.Price,
+                game.ReleaseDate
             );
 
-            games.Add(game);
-
-            return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id}, game);
+            return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, gameDto);
         });
 
         //PUT /games/1
